8fa21ddf9ef0edd4faa05a4cc39efe9b5a6d9316
xdemocle <xdemocle@gmail.com>
2022-07-26 10:47:24 +0200

    unify crons

--- src/utilities/cronlist.ts
+++ src/utilities/cronlist.ts
@@ -1,21 +1,16 @@
-import cron from 'node-cron';
 import graphQLClient from './grapql-client';
 // import logger from './logger';
 import { queryCheckAndExecuteOrderQueue, queryImportAndCheckOrders } from './queries';
 
 export const runCron = () => {
   // importAndCheckOrders
-  cron.schedule('*/5 * * * * *', async () => {
+  setInterval(async () => {
     await graphQLClient.request(queryImportAndCheckOrders);
-
-    setTimeout(async () => {
-      await graphQLClient.request(queryCheckAndExecuteOrderQueue);
-    }, 1500)
     // logger.debug(`importAndCheckOrders: ${importAndCheckOrders}`);
-    // logger.debug(`importAndCheckOrders: ${importAndCheckOrders}`);
-  });
+  }, 5000);
 
-  // Orders queue processing
-  // cron.schedule('*/15 * * * * *', async () => {
-  // });
+  setInterval(async () => {
+    await graphQLClient.request(queryCheckAndExecuteOrderQueue);
+    // logger.debug(`importAndCheckOrders: ${importAndCheckOrders}`);
+  }, 15000);
 }
