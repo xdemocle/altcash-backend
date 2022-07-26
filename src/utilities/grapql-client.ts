731af3f39f8d8720ca795a2eab11f5e0d0b16884
xdemocle <xdemocle@gmail.com>
2022-07-26 10:50:58 +0200

    add SERVER_HTTP_PORT for graphql

--- src/utilities/grapql-client.ts
+++ src/utilities/grapql-client.ts
@@ -1,5 +1,6 @@
 import { GraphQLClient, gql } from 'graphql-request';
+import { SERVER_HTTP_PORT } from '../config';
 
 export { gql }
 
-export default new GraphQLClient('http://localhost:4000/graphql');
+export default new GraphQLClient(`http://localhost:${SERVER_HTTP_PORT}/graphql`);
