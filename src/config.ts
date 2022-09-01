f04262bb07384a12f30be141370389185bec07ec
xdemocle <xdemocle@gmail.com>
2022-09-01 09:39:52 +0200

    force env . file

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
+import path from 'path';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
 export const NODE_ENV = process.env.NODE_ENV;
 
